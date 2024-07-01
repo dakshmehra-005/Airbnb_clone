da392af07f2b3f21cd4615f50e2f77e906c34148
dakshmehra-005 <160742687+dakshmehra-005@users.noreply.github.com>
2024-07-01 20:50:51 +0530

    Add files via upload

---
+++ models/review.js
@@ -0,0 +1,19 @@
+const { date } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  comment: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max:5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+module.exports = mongoose.model("Review", reviewSchema);
